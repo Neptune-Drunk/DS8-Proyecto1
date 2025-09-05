e34e7ae6e489eec937d053c2d6f58cad9b1ae404
Neptune-Drunk <vejaranobsebastian2@gmail.com>
2025-09-05 16:42:42 -0500

    Pantalla de Insertar archivo .dat

---
+++ Proyecto1/Form1.Designer.vb
@@ -0,0 +1,119 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.MenuToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.InsertarToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ConsultaToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SalirToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.BttnInsertar = New System.Windows.Forms.Button()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.Bttnarchivo = New System.Windows.Forms.Button()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.ImageScalingSize = New System.Drawing.Size(20, 20)
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.MenuToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(800, 28)
+        Me.MenuStrip1.TabIndex = 0
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'MenuToolStripMenuItem
+        '
+        Me.MenuToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.InsertarToolStripMenuItem, Me.ConsultaToolStripMenuItem, Me.SalirToolStripMenuItem})
+        Me.MenuToolStripMenuItem.Name = "MenuToolStripMenuItem"
+        Me.MenuToolStripMenuItem.Size = New System.Drawing.Size(60, 24)
+        Me.MenuToolStripMenuItem.Text = "Menu"
+        '
+        'InsertarToolStripMenuItem
+        '
+        Me.InsertarToolStripMenuItem.Name = "InsertarToolStripMenuItem"
+        Me.InsertarToolStripMenuItem.Size = New System.Drawing.Size(224, 26)
+        Me.InsertarToolStripMenuItem.Text = "Insertar"
+        '
+        'ConsultaToolStripMenuItem
+        '
+        Me.ConsultaToolStripMenuItem.Name = "ConsultaToolStripMenuItem"
+        Me.ConsultaToolStripMenuItem.Size = New System.Drawing.Size(224, 26)
+        Me.ConsultaToolStripMenuItem.Text = "Consulta"
+        '
+        'SalirToolStripMenuItem
+        '
+        Me.SalirToolStripMenuItem.Name = "SalirToolStripMenuItem"
+        Me.SalirToolStripMenuItem.Size = New System.Drawing.Size(224, 26)
+        Me.SalirToolStripMenuItem.Text = "Salir"
+        '
+        'BttnInsertar
+        '
+        Me.BttnInsertar.Location = New System.Drawing.Point(354, 278)
+        Me.BttnInsertar.Name = "BttnInsertar"
+        Me.BttnInsertar.Size = New System.Drawing.Size(131, 49)
+        Me.BttnInsertar.TabIndex = 1
+        Me.BttnInsertar.Text = "Insertar"
+        Me.BttnInsertar.UseVisualStyleBackColor = True
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'Bttnarchivo
+        '
+        Me.Bttnarchivo.Location = New System.Drawing.Point(78, 283)
+        Me.Bttnarchivo.Name = "Bttnarchivo"
+        Me.Bttnarchivo.Size = New System.Drawing.Size(129, 44)
+        Me.Bttnarchivo.TabIndex = 2
+        Me.Bttnarchivo.Text = "Adjuntar Archivo"
+        Me.Bttnarchivo.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Bttnarchivo)
+        Me.Controls.Add(Me.BttnInsertar)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents MenuToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents InsertarToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ConsultaToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents SalirToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents BttnInsertar As Button
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents Bttnarchivo As Button
+End Class
---
+++ Proyecto1/Form1.vb
@@ -0,0 +1,83 @@
+﻿Imports System.Data.SqlClient
+Imports System.Windows.Forms.VisualStyles.VisualStyleElement
+Imports MySql.Data.MySqlClient
+
+Public Class Form1
+    Dim archivo As String
+    Dim cm As MySqlCommand
+    Dim pr As MySqlDataAdapter
+    Dim ds1 As DataSet
+    Dim conexion As String = "Server=localhost;database=asistencia;user=root;password='';"
+    Dim miconexion As New MySqlConnection(conexion)
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Try
+            miconexion.Open()
+            cm = New MySqlCommand()
+            cm.CommandType = CommandType.Text
+            cm.Connection = miconexion
+            pr = New MySqlDataAdapter(cm)
+            ds1 = New DataSet()
+            pr.Fill(ds1)
+        Catch ex As Exception
+            MsgBox("Error de Conexión")
+        End Try
+    End Sub
+
+    Private Sub Bttnarchivo_Click(sender As Object, e As EventArgs) Handles Bttnarchivo.Click
+        Dim openFileDialog As New OpenFileDialog()
+        openFileDialog.Title = "Seleccione el archivo"
+        openFileDialog.Filter = "Archivos DAT (*.dat)|*.dat"
+
+        If openFileDialog.ShowDialog() = DialogResult.OK Then
+            archivo = openFileDialog.FileName
+            MessageBox.Show("Archivo seleccionado: " & archivo)
+        End If
+    End Sub
+
+    Private Sub BttnInsertar_Click(sender As Object, e As EventArgs) Handles BttnInsertar.Click
+        If String.IsNullOrEmpty(archivo) Then
+            MessageBox.Show("Por favor, seleccione un archivo primero.")
+            Return
+        End If
+
+        Dim lines() As String = System.IO.File.ReadAllLines(archivo)
+        Dim conexionbase As String = "Server=localhost;database=asistencia;user=root;password='';"
+
+        For Each line As String In lines
+            Dim parts() As String = line.Split(vbTab)
+            If parts.Length >= 2 Then
+                Dim id As String = parts(0).Trim()
+                Dim fechaHora As String = parts(1).Trim()
+                Dim fecha As String = ""
+                Dim hora As String = ""
+
+                Dim fechaHoraParts() As String = fechaHora.Split(" "c)
+                If fechaHoraParts.Length = 2 Then
+                    fecha = fechaHoraParts(0)
+                    hora = fechaHoraParts(1)
+                Else
+                    MessageBox.Show("Formato de fecha y hora inválido en la línea: " & line)
+                    Continue For
+                End If
+
+                Try
+                    Using miconexion As New MySqlConnection(conexionbase)
+                        miconexion.Open()
+                        Dim query As String = "INSERT INTO asistencia (id, fecha, hora) VALUES (@id, @fecha, @hora)"
+                        Using cmd As New MySqlCommand(query, miconexion)
+                            cmd.Parameters.AddWithValue("@id", id)
+                            cmd.Parameters.AddWithValue("@fecha", fecha)
+                            cmd.Parameters.AddWithValue("@hora", hora)
+                            cmd.ExecuteNonQuery()
+                        End Using
+                    End Using
+                Catch ex As Exception
+                    MessageBox.Show("Error al insertar el registro: " & ex.Message)
+                End Try
+            Else
+                MessageBox.Show("Línea inválida en el archivo: " & line)
+            End If
+        Next
+        MessageBox.Show("Datos insertados correctamente.")
+    End Sub
+End Class
